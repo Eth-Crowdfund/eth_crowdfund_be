9cfc2b8a2c93e002615829ef2cb4f94bc18d4614
Antonio Jackson <antoniojackson@gmail.com>
2020-09-07 18:46:19 -0600

    py env updates

--- migrations/env.py
+++ migrations/env.py
@@ -3,7 +3,7 @@ from __future__ import with_statement
 import logging
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, create_engine
 from sqlalchemy import pool
 
 from alembic import context
@@ -53,7 +53,10 @@ def run_migrations_offline():
     with context.begin_transaction():
         context.run_migrations()
 
-
+def get_url():
+    db = os.getenv('DATABASE_URL')
+    return db
+    
 def run_migrations_online():
     """Run migrations in 'online' mode.
 
@@ -72,11 +75,13 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info('No changes in schema detected.')
 
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix='sqlalchemy.',
-        poolclass=pool.NullPool,
-    )
+    # connectable = engine_from_config(
+    #     config.get_section(config.config_ini_section),
+    #     prefix='sqlalchemy.',
+    #     poolclass=pool.NullPool,
+    # )
+
+    connectable = create_engine(get_url()):
 
     with connectable.connect() as connection:
         context.configure(
